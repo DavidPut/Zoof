b382c914f65dfdd158c50332a9f129849d7bdf17
DavidPut <David_put@hotmail.com>
2015-04-16 14:26:04 +0200

    Kleuren

--- app/src/main/java/com/example/zoof/zoofzoof/MainActivity.java
+++ app/src/main/java/com/example/zoof/zoofzoof/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import org.json.JSONArray;
@@ -38,6 +39,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         //Search
         handleIntent(getIntent());
