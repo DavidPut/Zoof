003a4d207204e2bbcb83bfbd8aa8f13f97ef8668
DavidPut <David_put@hotmail.com>
2015-04-16 12:15:14 +0200

    Laast werkende versie

--- app/src/main/java/com/example/zoof/zoofzoof/MainActivity.java
+++ app/src/main/java/com/example/zoof/zoofzoof/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.zoof.zoofzoof;
 
+
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +19,7 @@ import org.json.JSONObject;
 import java.util.concurrent.ExecutionException;
 import AsyncTasks.LoadPicturesTask;
 import AsyncTasks.PictureGetTask;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -92,6 +97,7 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
+       
         return true;
     }
 
