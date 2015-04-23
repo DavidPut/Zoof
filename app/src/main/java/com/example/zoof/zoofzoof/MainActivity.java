cb1e96e698ba7920dfd35c1cdd1c97bf161e5b82
DavidPut <David_put@hotmail.com>
2015-04-23 12:27:20 +0200

    Icons

--- app/src/main/java/com/example/zoof/zoofzoof/MainActivity.java
+++ app/src/main/java/com/example/zoof/zoofzoof/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.CountDownTimer;
 import android.provider.Settings;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -270,8 +271,13 @@ public class MainActivity extends ActionBarActivity {
                 (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView =
                 (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
+           searchView.setSearchableInfo(
                 searchManager.getSearchableInfo(getComponentName()));
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.ic_launcher);
 
         return true;
     }
