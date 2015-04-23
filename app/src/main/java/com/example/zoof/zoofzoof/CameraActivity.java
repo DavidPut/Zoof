1b12fcbeee0461b7cda5efa2cee5b77af6be9f95
SEALdvd <dannyvandalen@145-24-243-148.wlan.hro.nl>
2015-04-23 11:36:44 +0200

    Deleted old menu stuff

--- app/src/main/java/com/example/zoof/zoofzoof/CameraActivity.java
+++ app/src/main/java/com/example/zoof/zoofzoof/CameraActivity.java
@@ -124,12 +124,6 @@ public class CameraActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
